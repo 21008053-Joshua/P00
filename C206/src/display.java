2221f4d580b6cb4775574669b1400726ceddea4c
mjsjo <mjsjo@LAPTOP-3JNESODR>
2022-05-27 16:03:19 +0800

    Testing

---
+++ C206/src/display.java
@@ -0,0 +1,24 @@
+/*
+ * I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: Joshua
+ * Student ID: 21008053
+ * Class: C208
+ * Date/Time created: 21/10/2021
+ */
+
+/**
+ *
+ */
+public class display {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("My sample Java Progam");
+
+	}
+
+}
